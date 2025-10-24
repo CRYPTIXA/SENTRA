868afc93be7024c160ff66161519d1e1b7669fd9
SWORD-SAINT69 <gotuhamsankar@aol.com>
2025-10-24 10:55:44 +0530

    config_loader.py

---
+++ src/config_loader.py
@@ -0,0 +1,26 @@
+import yaml
+import os
+
+def load_config(file_path="config/config.yml"):
+
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(f"File {file_path} not found")
+
+    with open(file_path, "r") as f:
+        config = yaml.safe_load(f)
+
+
+    return config
+
+config = load_config()
+
+telegram_token = config["telegram_token"]
+whale_threashold = config["whale_threashold_usd"]
+
+rpc_urls = config.get("rpc_urls", {})
+infura_url = rpc_urls.get("infura")
+alchemy_url = rpc_urls.get("alchemy")
+
+
+nhost_api_url = config["nhost"]["api_url"]
+nhost_admin_secret = config["nhost"]["admin_secret"]
